35cc72b0428eff75f0e60f3c2b1d4c4f51e49660
giraaaaaa <landamaizer@gmail.com>
2019-09-19 11:59:26 +0900

    complete

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,40 @@
 import React from "react";
-import { Query } from "react-apollo";
+import styled from "styled-components";
+import { Helmet } from "react-helmet";
 import { HOME_PAGE } from "./queries";
-const Home = () => (
-  <Query query={HOME_PAGE}>
-    {({ loading, data, error }) => {
-      if (loading) return <span>loading</span>;
-      if (error) return <span>Something happend</span>;
-      if (data) {
-        console.log(data);
-        return <span>yay</span>;
-      }
-    }}
-  </Query>
-);
+import Movie from "./Movie";
+import { useQuery } from "react-apollo-hooks";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 0.7fr);
+  flex-wrap: wrap;
+  justify-items: center;
+`;
+
+const Home = () => {
+  const { data, error, loading } = useQuery(HOME_PAGE);
+  return (
+    <Container>
+      <Helmet>
+        <title>Home | MovieQL</title>
+      </Helmet>
+      {loading && "Loading"}
+      {error && "Something is wrong"}
+      {!loading &&
+        data &&
+        data.movies &&
+        data.movies.map(movie => (
+          <Movie
+            id={movie.id}
+            key={movie.id}
+            poster={movie.medium_cover_image}
+            title={movie.title}
+            rating={movie.rating}
+          />
+        ))}
+    </Container>
+  );
+};
 
 export default Home;
